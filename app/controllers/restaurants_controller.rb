9ebb55d8c8cc64a1d8b2e2d321a1f5c1b24f5433
Alice Teilhard <alice.teilhard@gmail.com>
2022-11-15 23:21:35 +0000

    index route restaurant works

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,10 +14,21 @@ class RestaurantsController < ApplicationController
   def create
     restaurant = Restaurant.new(task_params)
     restaurant.save
-    # redirect_to restaurant_path
+    # redirect_to restaurant_path?
   end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :category)
   end
 end
+
+
+
+
+
+# need a route so that a visitor can see the list of all restaurants
+#   get "restaurants", to: "restaurants#index"
+
+
+# need a route so visitor can add a new restaurant, and be redirected to the show view of the new restaurant
+# need a route so the visitor can see the details of a restaurant with all the reviews related to the restaurant
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,8 +15,6 @@ end
 
 
 
-# a review must belong to a restaurant
-# a review must have content
-# a review must have rating
-# a review's rating must be a number between 0 and 5
-# a review's rating must be an integer -2.5 = invalid
+
+
+# need a route so the visitor can add a new review to a restaurant
